c742b6e7f22aeaffbc353b2d166f355dbf858224
kaziNAHIAN <provat44.knhp@gmail.com>
2023-12-31 20:44:39 +0600

    models updated

--- thesis_project_management/models.py
+++ thesis_project_management/models.py
@@ -7,6 +7,9 @@ class Department(models.Model):
     code = models.CharField(max_length=15)
     year_established = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.name} since {self.year_established}'
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
@@ -17,6 +20,12 @@ class Course(models.Model):
     course_type = models.CharField(max_length=50)
     general_scope = models.JSONField(max_length=500)
 
+    class Meta:
+        unique_together = ('title', 'code', 'department')
+
+    def __str__(self):
+        return f'{self.code} credit: {self.credit}'
+
 
 class Teacher(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -25,19 +34,31 @@ class Teacher(models.Model):
     expertise = models.CharField(max_length=100)
     publications = models.JSONField(max_length=1500)
 
+    class Meta:
+        unique_together = ('name', 'department')
+
+    def __str__(self):
+        return f'{self.name} dept. {self.department}'
+
 
 class Supervisor(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    department = models.ForeignKey(Department, related_name='supervisor', on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, related_name='supervisor', on_delete=models.CASCADE, null=True)
     expertise = models.CharField(max_length=100)
     publications = models.JSONField(max_length=1500)
+
+    def __str__(self):
+        return f'{self.name} dept. {self.department}'
 
 
 class Student(models.Model):
     registration = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     department = models.ForeignKey(Department, related_name='student', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'name: {self.name} dept: {self.registration} reg: {self.registration}'
 
 
 class Team(models.Model):
@@ -51,6 +72,12 @@ class Team(models.Model):
     assigned_supervisors = models.JSONField(max_length=100)
     status = models.JSONField(max_length=50)
     all_links = models.JSONField(max_length=2500)
+
+    class Meta:
+        unique_together = ('name', 'year', 'course')
+
+    def __str__(self):
+        return f'team: {self.name} of year: {self.year}'
 
 
 class TeamProgress(models.Model):
@@ -70,6 +97,9 @@ class Arrangement(models.Model):
     odd_semester = models.BooleanField()
     course_teacher = models.ForeignKey(Teacher, related_name='arrangement', on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('course', 'year', 'odd_semester')
+
 
 class Participation(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -78,3 +108,6 @@ class Participation(models.Model):
     remarks = models.CharField(max_length=500)
     result = models.CharField(max_length=20)
     artifacts = models.JSONField(max_length=200)
+
+    class Meta:
+        unique_together = ('arrangement', 'team')
